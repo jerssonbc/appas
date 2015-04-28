a1abb6e3131fc80e454621c3a91667cd955874bf
Jersson Bacilio <jerss.edu@gmail.com>
2015-04-28 18:27:04 -0500

    Agregue estilos de plantilla y incie el layout

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Applicación Auditoria</title>
+	<!-- Bootstrap core CSS -->
+	{{HTML::style('css/bootstrap.css')}}
+	<!-- external css-->
+	{{HTML::style('css/zabuto_calendar.css')}}
+	{{HTML::style('css/gritter/jquery.gritter.css')}}
+	{{HTML::style('css/lineicons/style.css')}}
+	<!-- Custom styles for this template -->
+	{{HTML::style('css/style.css')}}
+	{{HTML::style('css/style-responsive.css')}}
+
+</head>
+<body>
+	<section id="container">
+		<header class="header black-bg">
+			<div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+            </div>
+            <a href="index.html" class="logo"><b>App Auditoria</b></a>
+
+		</header>
+	</section>
+</body>
+</html>
